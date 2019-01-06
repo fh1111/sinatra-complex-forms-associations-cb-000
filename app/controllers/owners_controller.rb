2e79cd7405543dbac2bbb6f50fc52f386766c5f8
Fer Hernandez <fh.1111@gmail.com>
2019-01-06 21:30:23 +0000

    Automatically backed up by Learn

--- app/controllers/owners_controller.rb
+++ app/controllers/owners_controller.rb
@@ -13,7 +13,7 @@ class OwnersController < ApplicationController
   post '/owners' do
     @owner = Owner.create(params[:owner])
     redirect "owners/#{@owner.id}"
-    
+
     end
     redirect "owners/#{@owner.id}"
   end
